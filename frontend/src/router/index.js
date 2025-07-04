f1fd8a43e578178e96bd98f9169f3597b34b4d91
SURAAJ24 <22f3002242@ds.study.iitm.ac.in>
2025-07-05 01:06:53 +0530

    accept change

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,14 +9,6 @@ import SignupView from '../views/Signup.vue';
 import ProfileView from '../views/Profile.vue';
 import UserSummaryView from '../views/UserSummary.vue';
 import BuyPremiumView from '../views/BuyPremium.vue';
-<<<<<<< HEAD
-import ExpenseTrackerView from '../views/ExpenseTracker.vue';
-import ExpenseInterfaceView from '../views/ExpenseInterface.vue';
-import AddExpenseView from '../views/AddExpense.vue';
-import Lesson from '@/components/Lesson.vue';
-import Module from '@/components/Module.vue';
-import Topic from '@/components/Topic.vue';
-=======
 import AddContent from '../views/AddContent.vue';
 import AddModule from '../views/AddModule.vue';
 import AddTopic from '../views/AddTopic.vue';
@@ -24,7 +16,6 @@ import GenerateQuiz from '../views/GenerateQuiz.vue';
 import Lesson from '../views/Lesson.vue';
 import Module from '../views/Module.vue';
 import Topic from '../views/Topic.vue';
->>>>>>> main
 import store from '../store';
 
 const routes = [
@@ -34,60 +25,6 @@ const routes = [
   { path: '/contact', name: 'Contact', component: ContactView },
   { path: '/login', name: 'Login', component: LoginView },
   { path: '/signup', name: 'Signup', component: SignupView },
-<<<<<<< HEAD
-  {
-    path: '/user-dashboard',
-    name: 'UserDashboard',
-    component: UserDashboard,
-    meta: { requiresAuth: true, role: 'user' },
-  },
-  {
-    path: '/admin-dashboard',
-    name: 'AdminDashboard',
-    component: AdminDashboard,
-    meta: { requiresAuth: true, role: 'admin' },
-  },
-  {
-    path: '/profile',
-    name: 'Profile',
-    component: ProfileView,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/expense-tracker',
-    name: 'ExpenseTracker',
-    component: ExpenseTrackerView,
-    meta: { requiresAuth: true ,role: 'user'}, 
-  },
-  {
-    path: '/expense-interface',
-    name: 'ExpenseInterface',
-    component: ExpenseInterfaceView,
-    meta: { requiresAuth: true, role: 'user' }, 
-  },
-  {
-    path: '/add_expense',
-    name: 'AddExpense',
-    component: AddExpenseView,
-    meta: { requiresAuth: true, role: 'user' }, 
-  },
-  {
-    path: '/buy-premium',
-    name: 'BuyPremium',
-    component: BuyPremiumView,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/user-summary',
-    name: 'UserSummary',
-    component: UserSummaryView,
-    meta: { requiresAuth: true, role: 'user' },
-  },
-  { path: '/lesson', component: Lesson, name: 'Lesson' },
-  { path: '/lesson/:id', component: Module, name: 'Module', props: true },
-  { path: '/lessons/:lessonId/:topicId/learn_topic',component: Topic,name: 'LearnTopic'},
-  { path: '/:pathMatch(.*)*', name: 'NotFound', component: HomeView },
-=======
 
 
   { path: '/user-dashboard',name: 'UserDashboard',component: UserDashboard,meta: { requiresAuth: true, role: 'user' },},
@@ -108,7 +45,6 @@ const routes = [
   { path: '/add-content', name: 'AddContent', component: AddContent, meta: { requiresAuth: true, role: 'admin' } },
   { path: '/generate-quiz', name: 'GenerateQuiz', component: GenerateQuiz, meta: { requiresAuth: true, role: 'admin' } },
 
->>>>>>> main
 ];
 
 const router = createRouter({
